a195ee255a1bb19d0b8ff5b9607775d0f8635a91
Christopher Huston <chusto2@gmail.com>
2019-12-23 16:32:43 -0600

    added Routing to App component, needs to be filled in with info, added some boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={} />
+        </Switch>
+      </Router>  
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
